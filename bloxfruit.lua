e8631850eb93d5f5ecf04a26d1ae43d4066bdc7b
DarkZone888 <72551536+DarkZone888@users.noreply.github.com>
2023-11-18 23:44:24 +0700

    Update bloxfruit.lua

--- bloxfruit.lua
+++ bloxfruit.lua
@@ -328,7 +328,7 @@ local function AwakeAlias()
                             end
                         end
                     else
-                        AwakeText = string.split(.." "..game:GetService("Players").LocalPlayer.Data.DevilFruit.Value,"-")[2]
+                        AwakeText = string(.." "..game:GetService("Players").LocalPlayer.Data.DevilFruit.Value,"-")
                     end
                 elseif game:GetService("Players").LocalPlayer.Data.DevilFruit.Value == '' then
                     AwakeText = "ไม่มีผล"
@@ -371,7 +371,7 @@ local function AwakeAlias()
                             end
                         end
                     else
-                        AwakeText = string.split(.." "..game:GetService("Players").LocalPlayer.Data.DevilFruit.Value,"-")[2]
+                        AwakeText = string(.." "..game:GetService("Players").LocalPlayer.Data.DevilFruit.Value,"-")
                     end
                 end
             end
